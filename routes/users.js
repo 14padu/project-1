3e424619479f84ee7b5d4d7c2b8152055d5151c1
14padu <padmaja.phn@gmail.com>
2024-09-12 15:54:26 +0530

    finished the whole project

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {v4 as uuidv4}  from "uuid";
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // Mock database
 // const users = [
@@ -30,5 +30,54 @@ router.get('/', (req, res) => {
     users.push({...user,id:uuidv4()});
     res.send(`${user.first_name} has been added to the database`);
  })
+ router.get('/:id',(req,res) =>{
+    const { id } =req.params;
 
+    const foundUser = users.find((user) => users.id === id);
+     if (foundUser) {
+        res.send(foundUser);
+        
+     }else{
+        res.status(404).send('user not found')
+     }
+    
+ })
+ router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    users = users.filter((user) => user.id !== id)
+  
+    res.send(`${id} deleted successfully from database`);
+  })
+
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    users = users.filter((user) => user.id !== id)
+  
+    res.send(`${id} deleted successfully from database`);
+  });
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id)
+
+    res.send(foundUser)
+})
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    const { first_name, last_name, email} = req.body;
+  
+    const user = users.find((user) => user.id === id)
+  
+    if(first_name) user.first_name = first_name;
+    if(last_name) user.last_name = last_name;
+    if(email) user.email = email;
+  
+    res.send(`User with the ${id} has been updated`)
+  
+  });
 export default router
